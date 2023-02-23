dfb4e32e3b64f2016ce5946214e7a52fda7e45b8
Taylor Seamans <taylor.seamans@yahoo.com>
2023-02-23 14:37:31 -0800

    Add Button

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,28 @@
+import { forwardRef } from "react";
+import { button } from "grommet-exp-theme";
+
+type ButtonProps = {
+  icon?: boolean;
+  label?: string;
+  size?: "small" | "medium" | "large";
+  kind?: "default" | "secondary" | "primary";
+};
+
+const Button = forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ icon, kind, label, size, ...rest }: ButtonProps, ref) => {
+    return (
+      <button
+        className={button({ kind: kind, size: size })}
+        type="button"
+        ref={ref}
+        {...rest}
+      >
+        {label}
+      </button>
+    );
+  }
+);
+
+Button.displayName = "Button";
+
+export { Button };
