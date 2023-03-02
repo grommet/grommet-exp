7bee23cc627eb641d39cea40f0b4b60aa0ba2ad1
Eric Soderberg <eric.soderberg@hpe.com>
2023-03-02 09:22:28 -0800

    Enhanced Box

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,35 +1,77 @@
-import { forwardRef } from 'react';
-import { box } from 'grommet-exp-theme';
+import { forwardRef } from "react";
+import { box } from "grommet-exp-theme";
 
-type SizeType = 'small' | 'medium' | 'large'
-type AlignType = 'start' | 'center' | 'stretch' | 'end'
-type JustifyType = 'start' | 'center' | 'between' | 'end'
+type SizeType = "small" | "medium" | "large";
+type AlignType = "start" | "center" | "stretch" | "end";
+type JustifyType = "start" | "center" | "between" | "end";
 
-type SpacingType = SizeType
+type SpacingType = SizeType;
 // type SpacingType = SizeType | { horizontal?: SizeType, vertical?: SizeType }
 
 type BoxProps = {
   align?: AlignType;
+  background?: "base" | "back" | "front" | "contrast";
+  border?: boolean;
   children?: React.ReactNode;
-  direction?: 'row' | 'column';
-  gap?: SizeType;
+  direction?: "row" | "column";
+  elevation?: SizeType;
+  flex?: boolean | "grow" | "shrink";
+  gap?: SpacingType;
+  height?: SizeType;
   justify?: JustifyType;
   margin?: SpacingType;
   pad?: SpacingType;
-}
+  round?: SpacingType;
+  width?: SizeType;
+  wrap?: boolean;
+};
 
 const Box = forwardRef<HTMLDivElement, BoxProps>(
-  ( { align, direction = 'column', gap, justify, margin, pad, ...rest } : BoxProps, ref) => {
+  (
+    {
+      align,
+      background,
+      border,
+      direction = "column",
+      elevation,
+      flex,
+      gap,
+      height,
+      justify,
+      margin,
+      pad,
+      round,
+      width,
+      wrap,
+      ...rest
+    }: BoxProps,
+    ref
+  ) => {
     return (
       <div
-        className={box({ align, direction, gap, justify, margin, pad })}
+        className={box({
+          align,
+          background,
+          border,
+          direction,
+          elevation,
+          flex,
+          gap,
+          height,
+          justify,
+          margin,
+          pad,
+          round,
+          width,
+          wrap,
+        })}
         ref={ref}
         {...rest}
       />
-    )
-  },
-)
+    );
+  }
+);
 
-Box.displayName = 'Box';
+Box.displayName = "Box";
 
 export { Box };
