e76b11b1dc90c983628bcc62788ae4566688a3a5
Eric Soderberg <eric.soderberg@hpe.com>
2023-05-10 13:32:18 -0700

    Partial work on Button icon coloring

--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -2,13 +2,14 @@ import { cloneElement, forwardRef } from "react";
 import { icon } from "grommet-exp-theme";
 
 type IconProps = {
+  height?: "small" | "medium" | "large";
   size?: "small" | "medium" | "large" | "xlarge";
   svg: React.ReactElement<SVGSVGElement>;
 };
 
 const Icon = forwardRef<HTMLImageElement, IconProps>(
-  ({ size, svg, ...rest }: IconProps, ref): JSX.Element => {
-    return cloneElement(svg, { className: icon({ size }), ...rest });
+  ({ height, size, svg, ...rest }: IconProps, ref): JSX.Element => {
+    return cloneElement(svg, { className: icon({ height, size }), ...rest });
   }
 );
 
